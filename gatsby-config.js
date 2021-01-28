22fb1191de61ff6ed898ae3a02e3746c82c7d7cc
puppe1990 <matheus.puppe90@hotmail.com>
2021-01-28 16:59:48 -0300

    Remove unecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog Matheus Nunes Puppe`,
+    description: `A blog for developers and much more.`,
+    author: `@matheus.puppe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
