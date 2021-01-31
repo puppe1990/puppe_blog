088d0f0670b00a1058182502b65f7bdec8d31cde
puppe1990 <matheus.puppe90@hotmail.com>
2021-01-31 19:04:52 -0300

    Create profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matheus Nunes Puppe</h1>
+    <h2>Programador Backend</h2>
+    <p>Trabalho no eccomerce de moda feminina Purchase Store e desenvolvi um ERP para controlar estoque por QR Code.
+      Escrevo sobre programação, cloud, produtividade e livros.
+    </p>
+  </div>
+)
+
+export default Profile
